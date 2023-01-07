0d6a993416e11d44baa09cb0f6f91dea0b0c42de
trdmonkey <trdmonkey218@gmail.com>
2023-01-06 21:10:53 -0500

    primer cambio de prueba

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Mi primer proyecto</title>
+        <style>
+            #uno{
+                margin: 0 auto;
+                text-align: center;
+                padding: 30px;
+                padding-top: 10px;
+                height: 300px;
+                width: 800px;
+                background-color: whitesmoke;
+                border: 5px;
+                border-radius: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="uno">
+            <table>
+                <tr><th style="text-align: left">Nombres: </th><td>Jorge Luis Lopez Ramos</td></tr>
+                <tr><th style="text-align: left">Cedula: </th><td>1.085.253.456</td></tr>
+                <tr><th style="text-align: left">Programa de formación: </th><td>Desarrollo web con PHP</td></tr>
+            </table>
+            <br>
+            <b>Un saludo para nuestro instructor Milton</b><br>
+            <img src="https://archivos.territorio.la/fotos/clases/636db0c28a0a6.jpeg" alt="IMG" style="border: 1px solid black; border-radius: 10px; ">
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
